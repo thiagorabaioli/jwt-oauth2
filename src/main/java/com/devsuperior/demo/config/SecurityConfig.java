64af8d9f5f5da4c1b3496201e79094c5d018745d
thiagorabaioli <tfrabaioli@outlook.com>
2024-06-09 18:47:00 +0100

    implementação interface PasswordEncoder BCrypt

--- src/main/java/com/devsuperior/demo/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.devsuperior.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){ //Interface do BCrypt
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -14,5 +22,6 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); //Configura permissão para todos os endpoints
         return http.build();
     }
+
 
 }
