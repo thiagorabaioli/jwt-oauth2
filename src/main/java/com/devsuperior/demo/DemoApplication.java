30a79d869fdec3c62e80520649becc0a451d99c1
thiagorabaioli <tfrabaioli@outlook.com>
2024-06-09 19:26:17 +0100

    implementação interfaces UserDetails e GrandedAutority

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,21 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
-
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("ENCODE = " +passwordEncoder.encode("123456"));
 
-		boolean result = passwordEncoder.matches("123457","$2a$10$.BWqaq1.I.ixMpRdT.zxVeCTZFOZp6hPZ5nEYIyov/Mk0ZATbdkVy");
-		System.out.println(result);
-
-	}
 }
